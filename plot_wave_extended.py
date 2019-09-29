5c6e78292542a7001b01c609276d8ffcc403bf03
Muhammad Ryan <nayrhum4code@gmail.com>
2019-09-30 01:01:36 +0800

    Update plot_wave_extended.py

--- plot_wave_extended.py
+++ plot_wave_extended.py
@@ -14,15 +14,18 @@ frequency = 4
 total_time = 1
 res = 0.01
 
-def plot_data(x_data, y_data, x_label, y_label, fig_name, title='Sine Wave', y_lim=None):
+def plot_data(x_data, y_data, x_label, y_label, fig_name, title='Sine Wave', y_lim=None, plot_type='plot'):
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
 	ax.set_xlim(x_data[0], x_data[-1])
 	if y_lim != None:
-		ax.set_ylim(0, y_lim)
+		ax.set_ylim(y_lim[0], y_lim[1])
 	ax.set_ylabel(y_label)
 	ax.set_xlabel(x_label)
-	ax.stem(x_data, y_data)
+	if plot_type == 'plot':
+		ax.plot(x_data, y_data)
+	elif plot_type == 'stem':
+		ax.stem(x_data, y_data)
 	ax.set_title(title)
 	fig.savefig(fig_name)
 	plt.close(fig)
@@ -36,7 +39,7 @@ for i in range(extend_times):
 	amplitude_extended[i*len(amplitude):(i+1)*len(amplitude)] = amplitude
 
 #print(amplitude_extended)
-plot_data(time_extended, amplitude_extended, 'Time', 'Amplitude', 'sin_wave-extended.png', title='finished 4 Hz Sine Wave (Extended)')
+plot_data(time_extended, amplitude_extended, 'Time', 'Amplitude', 'sin_wave-extended.png', title='Finished 4 Hz Sine Wave (Extended)')
 
 #modified signal
 time = time[:-minus_step]
@@ -46,7 +49,7 @@ time_modified_extended = np.arange(0, total_time*extend_times, res)[:-minus_step
 amplitude_modified_extended = np.zeros(len(amplitude)*extend_times)
 for i in range(extend_times):
 	amplitude_modified_extended[i*len(amplitude):(i+1)*len(amplitude)] = amplitude
-plot_data(time_modified_extended, amplitude_modified_extended, 'Time', 'Amplitude', 'test-3.png', title='unfinished 4 Hz Sine Wave (Extended)')
+plot_data(time_modified_extended, amplitude_modified_extended, 'Time', 'Amplitude', 'sin_wave-modified-extended.png', title='Unfinished 4 Hz Sine Wave (Extended)')
 
 #apply fft
 fftdata = np.fft.fft(amplitude_modified_extended)
@@ -55,4 +58,5 @@ for i in range(len(fftdata)):
 	fftdatafreq[i] = abs(fftdata[i].real)
 
 plot_data(np.arange(0, len(time_modified_extended) // 2), fftdatafreq[:len(time_modified_extended) // 2], 
-'Frequency', 'Power', 'sin_wave_fft-modified-extended.png', title='FFT of Unfinished 4 Hz Sine Wave (extended)', y_lim=70)
+'Frequency', 'Power', 'sin_wave_fft-modified-extended.png', title='FFT of Unfinished 4 Hz Sine Wave (extended)',
+y_lim=[0,70], plot_type='stem')
