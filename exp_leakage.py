8231777a8f3950ba173a8050cfc9efadad7034d6
Muhammad Ryan <nayrhum4code@gmail.com>
2019-09-30 00:56:01 +0800

    Update exp_leakage.py

--- exp_leakage.py
+++ exp_leakage.py
@@ -13,15 +13,18 @@ frequency = 4
 total_time = 1
 res = 0.01
 
-def plot_data(x_data, y_data, x_label, y_label, fig_name, title='Sine Wave', y_lim=None):
+def plot_data(x_data, y_data, x_label, y_label, fig_name, title='Sine Wave', y_lim=None, plot_type='plot'):
 	fig = plt.figure()
 	ax = fig.add_subplot(111)
 	ax.set_xlim(x_data[0], x_data[-1])
 	if y_lim != None:
-		ax.set_ylim(0, y_lim)
+		ax.set_ylim(y_lim[0], y_lim[1])
 	ax.set_ylabel(y_label)
 	ax.set_xlabel(x_label)
-	ax.stem(x_data, y_data)
+	if plot_type == 'plot':
+		ax.plot(x_data, y_data)
+	elif plot_type == 'stem':
+		ax.stem(x_data, y_data)
 	ax.set_title(title)
 	fig.savefig(fig_name)
 	plt.close(fig)
@@ -29,7 +32,7 @@ def plot_data(x_data, y_data, x_label, y_label, fig_name, title='Sine Wave', y_l
 time = np.arange(0, total_time, res)
 amplitude = np.sin(2 * np.pi * frequency * total_time * (time/time[-1]))
 
-plot_data(time, amplitude, 'Time', 'Amplitude', 'sin_wave.png', title='4 Hz Sine Wave')
+plot_data(time, amplitude, 'Time', 'Amplitude', 'sin_wave.png', title='4 Hz Sine Wave', plot_type='stem')
 
 
 #apply fft
@@ -39,13 +42,13 @@ for i in range(len(fftdata)):
 	fftdatafreq[i] = abs(fftdata[i].real)
 
 plot_data(np.arange(0, len(time) // 2), fftdatafreq[:len(time) // 2], 
-'Frequency', 'Power', 'sin_wave_fft.png', title='FFT of 4 Hz Sine Wave', y_lim=20)
+'Frequency', 'Power', 'sin_wave_fft.png', title='FFT of 4 Hz Sine Wave', y_lim=[0,20], plot_type='stem')
 
 #modify
 time = time[:-minus_step]
 amplitude = amplitude[:-minus_step]
 
-plot_data(time, amplitude, 'Time', 'Amplitude', 'sin_wave-modified.png', title='Unfinished 4 Hz Sine Wave')
+plot_data(time, amplitude, 'Time', 'Amplitude', 'sin_wave-modified.png', title='Unfinished 4 Hz Sine Wave', plot_type='stem')
 
 #apply fft
 fftdata = np.fft.fft(amplitude)
@@ -54,4 +57,4 @@ for i in range(len(fftdata)):
 	fftdatafreq[i] = abs(fftdata[i].real)
 
 plot_data(np.arange(0, len(time) // 2), fftdatafreq[:len(time) // 2], 'Frequency', 
-'Power', 'sin_wave-modified_fft.png', title='FFT of Unfinished 4 Hz Sine Wave', y_lim=20)
+'Power', 'sin_wave-modified_fft.png', title='FFT of Unfinished 4 Hz Sine Wave', y_lim=[0,20], plot_type='stem')
